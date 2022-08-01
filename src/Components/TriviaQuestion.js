0de0abb798c0d276ff71d66f310d8ab84e4dfebe
Rishi Mishra <rishimishra244@gmail.com>
2022-08-01 10:28:46 +0530

    V1

---
+++ src/Components/TriviaQuestion.js
@@ -0,0 +1,90 @@
+import { useEffect, useState } from "react";
+import {
+  GiCrossMark,
+  GiCrossedAxes,
+  GiCheckMark,
+  GiCheckeredFlag,
+} from "react-icons/gi";
+import "./TriviaQuestion.css";
+import axios from "axios";
+
+const TriviaQuestion = () => {
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [correctAnswer, setCorrectAnswer] = useState("");
+  const [flag, setFlag] = useState(-1);
+
+  useEffect(() => {
+    getQuestionFromApi();
+  }, []);
+
+  //to get new question's data from api
+  const getQuestionFromApi = () => {
+    axios(`https://opentdb.com/api.php?amount=1`).then(
+      (response) => {
+        // console.log(response.data.results[0].correct_answer);
+        // setCorrectAnswer("sdfsfg   sdfsfsfsdf  s df sfs fsd sdf sdf sdfsdfdfssdf");
+        setQuestion(response.data.results[0].question);
+        setCorrectAnswer(response.data.results[0].correct_answer);
+      },
+      (error) => {
+        //in case of error
+      }
+    );
+  };
+  //to validate the answer when the user clicks on submit button
+  const validateAnswer = () => {
+    if (answer === "") {
+      alert("Answer should not be empty");
+      return;
+    } else if (correctAnswer === answer) {
+      setFlag(1);
+    } else {
+      setFlag(0);
+    }
+    setTimeout(() => {
+      getQuestionFromApi();
+      setFlag(-1);
+    }, 2000);
+  };
+  return (
+    <div className="container">
+      <div className="TriviaQuestion">
+        <h1>TRIVIA</h1>
+        <div className="question">
+          <h3>
+            Que.&nbsp;
+            {question.replace(/\&[#?a-z0-9]*\;/g, "")}
+          </h3>
+          <input
+            className="answer"
+            type="text"
+            onChange={(e) => setAnswer(e.target.value)}
+            value={answer}
+          />
+          <div className="submit">
+            <div className="options">
+              {flag === 1 && (
+                <>
+                  <GiCheckMark style={{ color: "green" }} />
+                  <p>Correct Ans: {correctAnswer}</p>
+                </>
+              )}
+              {flag === 0 && (
+                <>
+                  <GiCrossMark style={{ color: "red" }} />
+                  <p>Correct Ans: {correctAnswer}</p>
+                </>
+              )}
+            </div>
+            <button type="" onClick={validateAnswer}>
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TriviaQuestion;
